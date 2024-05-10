2d5cd44365c809f6ea41bc628b113552943afcb0
Eduardo Roseo <eduardoroseo@gmail.com>
2024-05-09 21:56:50 -0300

    test: criando matematicatest:

--- tests/MatematicaTest.php
+++ tests/MatematicaTest.php
@@ -23,6 +23,46 @@ class MatematicaTest extends TestCase
         $this->assertNotEquals(5, $resultado);
     }
 
+    public function test_subtrai_dois_numeros_com_sucesso()
+    {
+        $matematica = new Matematica();
+        $resultado = $matematica->subtrair(2, 2);
+
+        $this->assertEquals(0, $resultado);
+    }
+
+    public function test_subtrai_dois_numeros_com_erro()
+    {
+        $matematica = new Matematica();
+        $resultado = $matematica->subtrair(2, 2);
+
+        $this->assertNotEquals(1, $resultado);
+    }
+
+    public function test_multiplica_dois_numeros_com_sucesso()
+    {
+        $matematica = new Matematica();
+        $resultado = $matematica->multiplicar(2, 2);
+
+        $this->assertEquals(4, $resultado);
+    }
+
+    public function test_multiplica_dois_numeros_com_erro()
+    {
+        $matematica = new Matematica();
+        $resultado = $matematica->multiplicar(2, 2);
+
+        $this->assertNotEquals(5, $resultado);
+    }
+
+    public function test_divide_dois_numeros_com_sucesso()
+    {
+        $matematica = new Matematica();
+        $resultado = $matematica->dividir(2, 2);
+
+        $this->assertEquals(1, $resultado);
+    }
+
     public function test_divisao_por_zero()
     {
         $this->expectException(DivisionByZeroException::class);
@@ -30,5 +70,13 @@ class MatematicaTest extends TestCase
 
         $matematica = new Matematica();
         $matematica->dividir(2, 0);
+    }
+
+    public function test_divide_dois_numeros_com_erro()
+    {
+        $matematica = new Matematica();
+        $resultado = $matematica->dividir(2, 2);
+
+        $this->assertNotEquals(2, $resultado);
     }
 }
